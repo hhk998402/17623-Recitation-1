a2978dd8ee9eff1205f95c55367ac9b05d08c265
ROHIT1005 <rshreeni@andrew.cmu.edu>
2022-11-03 12:14:53 -0400

    initital

---
+++ src/test/java/com/areyes1/jgc/mockito/e/JUnitServiceTestExample.java
@@ -0,0 +1,60 @@
+package com.areyes1.jgc.mockito.e;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+/**
+ * The Class JUnitServiceTestExample.
+ */
+public class JUnitServiceTestExample {
+
+	/**
+	 * Test1.
+	 */
+	@Test
+	public void testSimpleInt() {
+		// create mock
+
+
+		// define return value for method getUniqueId()
+
+
+		// use mock in test....
+
+	}
+
+	/**
+	 * Test more than one return value.
+	 */
+	// Demonstrates the return of multiple values
+	@Test
+	public void testMoreThanOneReturnValue() {
+
+	}
+
+	@Test
+	public void testVerify() {
+		// create and configure mock
+
+
+		// call method testing on the mock with parameter 12
+
+
+		// now check if method testing was called with the parameter 12
+
+
+		// was the method called twice?
+
+
+		// other alternatives for verifiying the number of method calls for a
+		// method
+		
+	}
+
+}
